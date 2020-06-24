886489bbcd0b7dfeaba8242e0113e3c6abaafbe7
bojeee <rkovalyk@gmail.com>
2020-06-24 17:58:32 +0300

    First_HW_added

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 3;
+        short b = 85;
+        long c = 63l;
+        float d = 87.6f;
+        double e = 14.050;
+        System.out.println(a + b);
+        System.out.println(c / e);
+        System.out.println(d - a);
+        System.out.println(e * a);
+    }
+
+}
